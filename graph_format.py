4f13e874a5570ecf84fbd31e0cc48e9b8954c4ad
peterravn <pmv384@alumni.ku.dk>
2025-06-06 00:11:06 +0200

    PLR: Grafer plus dual annealing test

--- graph_format.py
+++ graph_format.py
@@ -976,6 +976,81 @@ def plot_model_vs_data_3x2(a_dict, title=None, save_title=None):
     plt.show()
 
 
+def plot_model_vs_data_2x3(a_dict, title=None, save_title=None):
+    
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    keys = list(a_dict.keys())
+    n_panels = len(keys)
+    ncols = 2
+    nrows = 2
+
+    fig, axes = plt.subplots(nrows, ncols, figsize=(12, 4 * nrows), sharex=False)
+    axes = axes.flatten()
+
+    for i, key in enumerate(keys):
+        ax = axes[i]
+        sim_data, empirical_data = a_dict[key]
+        T_sim = len(sim_data)
+        T_emp = len(empirical_data)
+
+        if key in ['hours', 'extensive']:
+            age_start, age_end = 30, 72
+        elif key in ['illiquid']:
+            age_start, age_end = 30, 100
+        elif key in ['wages']:
+            age_start, age_end = 30, 60
+        else:
+            age_start, age_end = 30, 100
+
+        time = np.arange(age_start, age_start + max(T_sim, T_emp))
+
+        ax.plot(time[:T_emp], empirical_data, label="Empirical", color=custom_palette[0], linewidth=2)
+        ax.plot(time[:T_sim], sim_data, label="Simulated", color=custom_palette[1], linestyle='--', linewidth=2)
+
+        ax.set_title(key.capitalize(), fontsize=12, fontweight="semibold")
+        ax.set_xlim(age_start, age_end)
+
+        if key == 'hours':
+            ax.set_ylim(0.2, 1)
+            ax.set_ylabel("Full time equivalent hours", fontsize=12)
+        elif key == 'extensive':
+            ax.set_ylim(0, 1)
+            ax.set_ylabel("Percent", fontsize=12)
+        elif key == 'liquid':
+            ax.set_ylabel("DKK", fontsize=12)
+        elif key == 'illiquid':
+            ax.set_ylim(0, 3)
+            ax.set_ylabel("Million DKK", fontsize=12)
+        elif key == 'wages':
+            ax.set_ylim(400_000, 600_000)
+            ax.set_ylabel("DKK", fontsize=12)
+
+        ax.grid(True, linestyle="--", alpha=0.6)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.legend(fontsize=10)
+        ax.set_xlabel("Age", fontsize=12)
+        ax.tick_params(labelsize=12)
+
+
+    # Hide unused axes
+    for j in range(n_panels, len(axes)):
+        fig.delaxes(axes[j])
+
+    if title:
+        fig.suptitle(title, fontsize=15, fontweight="bold", y=1.02)
+
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.93)
+    if save_title:
+        save_figure(fig, save_title)
+
+    plt.show()
+
+
+
 def plot_total_effect_with_trend(total_effect_list, title=None, save_title=None):
     from scipy.stats import linregress
     # Prepare data
